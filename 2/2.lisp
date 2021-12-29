045b91bf6657e896b58bf9b66fadd3941599e519
Noor <noordotkhan@gmail.com>
2021-12-29 15:29:57 -0500

    Update 2.lisp

--- 2/2.lisp
+++ 2/2.lisp
@@ -1,5 +1,6 @@
 (ql:quickload :cl-ppcre)
 
+;; part 1
 (defun calculate-position (input-file)
   (let ((horizontal-position 0)
 	(depth 0))
@@ -10,7 +11,6 @@
 	   (let* ((input (cl-ppcre:split "\\s+" line))
 		  (direction (nth 0 input))
 		  (distance (parse-integer (nth 1 input))))
-	     (print (concatenate 'string direction (write-to-string distance)))
 	     (cond ((equalp "forward" direction) (setf horizontal-position (+ horizontal-position distance)))
 		   ((equalp "down" direction) (setf depth (+ depth distance)))
 		   ((equalp "up" direction) (setf depth (- depth distance)))))))
@@ -18,7 +18,24 @@
 
 (calculate-position "input.txt")
 
-(write-to-string 5)
+;; part 2
+(defun calculate-correct-position (input-file)
+  (let ((horizontal-position 0)
+	(depth 0)
+	(aim 0))
+    (with-open-file (stream input-file)
+      (loop for line = (read-line stream nil)
+	 until (null line)
+	 do
+	   (let* ((input (cl-ppcre:split "\\s+" line))
+		  (direction (nth 0 input))
+		  (units (parse-integer (nth 1 input))))
+	     (cond ((equalp "forward" direction) (setf horizontal-position (+ horizontal-position units)
+						       depth (+ depth (* aim units))))
+		   ((equalp "down" direction) (setf aim (+ aim units)))
+		   ((equalp "up" direction) (setf aim (- aim units)))))))
+    (* horizontal-position depth)))
 
-(equalp "forward" "forward")
+(calculate-correct-position "test.txt")
+(calculate-correct-position "input.txt")
 
