f1bd6a9d733d8e580c3124eff6acf293aa966a9d
Noor <noordotkhan@gmail.com>
2021-12-30 13:55:56 -0500

    Add files via upload

--- 3/3.lisp
+++ 3/3.lisp
@@ -5,35 +5,35 @@
       (loop for line = (read-line stream nil)
 	 until (null line)
 	 do
-	   (loop for index from 0 to (- (length line) 1)
+	   (loop for index from 0 to (1- (length (write-to-string (parse-integer line))))
 	      do
 		(let ((current-char (char line index)))
 		  (if (null (nth index counts))
 		      (setf counts (append counts (list (list :0 0 :1 0)))))
-		  (if (eq #\0 current-char)
+		  (if (char= #\0 current-char)
 		      (incf (getf (nth index counts) :0))
 		      (incf (getf (nth index counts) :1)))))))
-    counts))
+    (* (get-gamma-rate counts) (get-epsilon-rate counts))))
+
+(defun get-gamma-rate (counts)
+  (binary-to-decimal
+   (reduce (lambda (x y) (concatenate 'string x (if (> (getf y :0) (getf y :1)) "0" "1")))
+	   counts :initial-value "")))
+
+(defun get-epsilon-rate (counts)
+  (binary-to-decimal
+   (reduce (lambda (x y) (concatenate 'string x (if (< (getf y :0) (getf y :1)) "0" "1")))
+	   counts :initial-value "")))
+
+(defun binary-to-decimal (binary-number)
+  (let ((decimal-number 0)
+	(exponent (1- (length binary-number))))
+    (loop for index from 0 to (- (length binary-number) 1)
+       do (setf decimal-number (+ decimal-number
+				  (* (digit-char-p (char binary-number index)) (expt 2 exponent))))
+	 (decf exponent))
+    decimal-number))
 
 (get-power-consumption "test.txt")
+(get-power-consumption "input.txt")
 
-(map nil #'princ "bacon")
-
-(apply '+ '(1 2 3))
-
-(defvar *list* (list :0 0 :1 1))
-
-
-(incf (getf *list* :0))
-
-(defvar *my-list* (list 1 2 3))
-
-(append *my-list* (list 4))
-*my-list*
-(append (list 1 2 3) (list 4))
-
-(nth 1 '())
-
-*list*
-
-(eq (char "0" 0) (char "1" 0))
