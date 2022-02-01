d78dd21e482b567ef8d3c843b5178c08dfd6cbc1
Noor Khan <noordotkhan@gmail.com>
2022-01-31 20:13:01 -0500

    test input works

--- 4/4.lisp
+++ 4/4.lisp
@@ -11,44 +11,66 @@
        (print line)))
 
 (defun get-score (input-file)
-  (let ((new-board nil)
-	(boards '())
+  (let ((boards '())
 	(numbers nil)
 	(return-count 0)
 	(current-board '()))
-    (with-open-file (stream "test.txt")
-     (loop for line = (read-line stream nil)
-	until (null line)
-	do
-	  (cond
-	    ((string= line #\Return)
-	     (incf return-count)
-	     (if (> return-count 0)
-		 (progn (setf boards (append (list current-board) boards))
-			(setf current-board '()))))
-	    ((= return-count 0)
-		 (print line)
-	     (setf numbers
-		   (map 'list
-			(lambda (n) (parse-integer n :junk-allowed t))
-			(cl-ppcre:split "," line))))
-	    (t
+    (with-open-file (stream input-file)
+      (loop for line = (read-line stream nil)
+	 until (null line) do
+	   (cond
+	     ((string= line #\Return)
+	      (incf return-count)
+	      (if (and (> return-count 0) (not (null current-board)))
+		  (progn (setf boards (append boards (list current-board)))
+			 (setf current-board '()))))
+	     ((= return-count 0)
+	      (setf numbers
+		    (map 'list
+			 (lambda (n) (parse-integer n :junk-allowed t))
+			 (cl-ppcre:split "," line))))
+	     (t
+	      (setf current-board (append
+				   current-board
+				   (list (map 'list (lambda (s) (parse-integer s))
+					      (cl-ppcre:split "\\s+" (string-left-trim '(#\Space) line))))))))))
+    (setf boards (append boards (list current-board)))
+    (let ((read-numbers '())
+	  (winning-number nil)
+	  (winning-board-index nil))
+      (loop named find-winning-board for n in numbers do
+	   (setf read-numbers (append read-numbers (list n)))
+	   (loop for i from 0 below (list-length boards) do
+		(loop for j from 0 below (list-length (nth i boards)) do
+		     (let ((current-line (nth j (nth i boards))))
+		       (if (subsetp current-line read-numbers)
+			   (progn
+			     (setf winning-number n)
+			     (setf winning-board-index i)
+			     (return-from find-winning-board)))))))
+      (print read-numbers)
+      (print winning-number)
+      (print winning-board-index)
+      (let ((sum-unmarked-numbers 0))
+	(loop for line in (nth winning-board-index boards) do
 	     (print line)
-	     (setf current-board (append
-	       (list (map 'list (lambda (s) (parse-integer s))
-			  (cl-ppcre:split "\\s+" (string-left-trim '(#\Space) line))))
-	       current-board))))))
-    (setf boards (append (list current-board) boards))
-    (print numbers)
-    (print boards)
-    (print (nth 5 boards))
-    (print (list-length boards))))
+	     (loop for n in line do
+		  (if (not (find n read-numbers)) (progn (print n) (incf sum-unmarked-numbers n)))))
+	(* winning-number sum-unmarked-numbers)))))
 
-(list-length '(1 2 3))
+(get-score "input.txt")
+(get-score "input.txt")
 
-(get-score "test.txt")
+(not nil)
 
-(append (list 1) '(1 2 3))
+(defparameter test 5)
+(incf test 5)
+
+(last '(1 2 3))
+
+(subsetp '(1 2 3) '(4 5 6 3 2 1))
+
+(append (list 1 4 5) '(1 2 3))
 
 (string= #\Return #\Return)
 
